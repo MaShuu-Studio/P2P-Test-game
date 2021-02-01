1b3f7bb81af164cfb9fdb410f506dc8ccfab74b3
Shuu <ghwls328@naver.com>
2021-02-01 21:39:15 +0900

    Basic Network System

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Character : NetworkBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody rigidbody;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.isLocalPlayer)
+        {
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+            float jump = 0;
+            if (Input.GetButtonDown("Jump") && rigidbody.velocity.y == 0) jump = 30;
+            rigidbody.velocity = new Vector3(speed * moveX, jump, speed * moveZ);
+        }
+    }
+}
